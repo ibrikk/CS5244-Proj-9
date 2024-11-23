3a8ec31eb5f4817a58712ea8f249a043c4f9e8bf
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-11-23 02:30:57 -0500

    work on be

--- server/app/src/main/java/business/customer/CustomerForm.java
+++ server/app/src/main/java/business/customer/CustomerForm.java
@@ -40,29 +40,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CustomerForm {
 
-    private String name;
+	private String name;
 
 	private String address;
 
-    private String phone;
+	private String phone;
 
 	private String email;
 
-    private String ccNumber;
+	private String ccNumber;
 
-    private String ccExpiryMonth;
+	private String ccExpiryMonth;
 
-    private String ccExpiryYear;
+	private String ccExpiryYear;
 
-    @JsonCreator
+	@JsonCreator
 	public CustomerForm(
-		@JsonProperty("name") String name,
-		@JsonProperty("address") String address,
-		@JsonProperty("phone") String phone,
-		@JsonProperty("email") String email,
-		@JsonProperty("ccNumber") String ccNumber,
-		@JsonProperty("ccExpiryMonth") String ccExpiryMonth,
-		@JsonProperty("ccExpiryYear") String ccExpiryYear) {
+			@JsonProperty("name") String name,
+			@JsonProperty("address") String address,
+			@JsonProperty("phone") String phone,
+			@JsonProperty("email") String email,
+			@JsonProperty("ccNumber") String ccNumber,
+			@JsonProperty("ccExpiryMonth") String ccExpiryMonth,
+			@JsonProperty("ccExpiryYear") String ccExpiryYear) {
 		this.name = name;
 		this.address = address;
 		this.phone = phone;
@@ -73,24 +73,24 @@ public class CustomerForm {
 	}
 
 	public String getName() {
-        return name;
-    }
+		return name;
+	}
 
 	public String getAddress() {
 		return address;
 	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
 	public String getEmail() {
 		return email;
 	}
 
 	public String getCcNumber() {
-        return ccNumber;
-    }
+		return ccNumber;
+	}
 
 	public String getCcExpiryMonth() {
 		return ccExpiryMonth;
