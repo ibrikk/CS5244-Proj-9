9fbdd0436195c27d4ebb51530a0a493bd9242774
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-11-23 01:35:22 -0500

    work on be

--- server/app/src/main/java/api/ApiApplication.java
+++ server/app/src/main/java/api/ApiApplication.java
@@ -6,13 +6,4 @@ import java.util.Set;
 
 @ApplicationPath("/api")
 public class ApiApplication extends Application {
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        resources.add(ApiResource.class);
-        resources.add(ApiExceptionHandler.class);
-        // Add other resources later
-        return resources;
-    }
 }
